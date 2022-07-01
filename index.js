f58f9eb7c01e4bfbd4d24d10fa675bb2f447ab73
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-07-01 17:43:15 +0600

    update task and status checkbox added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+//mongoDB
 const uri =
   "mongodb+srv://fahim:85D8NzAmBavQyJLr@cluster0.oaexq.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -16,12 +18,56 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 async function run() {
-    try {
-        await client.connect();
-        const collection = client.db("test").collection("devices");
-      } finally {
-        
-      }
+  try {
+    await client.connect();
+    const taskCollection = client.db("task-app").collection("tasks");
+
+    // post tasks
+    app.post("/addTask", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await taskCollection.insertOne(data);
+      res.send(result);
+
+      // get tasks
+      app.get("/tasks", async (req, res) => {
+        const query = {};
+        const cursor = taskCollection.find(query);
+        const tools = await cursor.toArray();
+        res.send(tools);
+      });
+    });
+
+    // update tasks
+    app.put("/upToDo/:id", async (req, res) => {
+      const id = req.params.id;
+      const todo = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {
+              data: todo.updateTodo
+          },
+      };
+      const Todo = await taskCollection.updateOne(filter, updateDoc, options);
+      res.send({ success: true, data: Todo });
+    });
+
+    // checkbox and complete task
+    app.patch("/Checkbox/:CheckID", async (req, res) => {
+      const data = req.params.CheckID;
+      const id = { _id: ObjectId(data) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          Checkbox: true,
+        },
+      };
+      const result = await taskCollection.updateOne(id, updateDoc, options);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
